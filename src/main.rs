97ffe182cb7e37a64d10882e1f6d478df82ec86f
Jordis Hollander <jordis1995@live.nl>
2018-12-20 14:19:59 +0100

    Make engine give an outcome on the web interface

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use inference_engine;
+use inference_engine::{self, Atom, Question};
 use rocket::http::RawStr;
-use rocket::request::Form;
-use rocket::request::FromForm;
+use rocket::request::FromFormValue;
 use rocket::response::Redirect;
 use rocket::{catch, catchers, get, routes, uri, Request};
 use rocket_contrib::serve::StaticFiles;
@@ -13,23 +12,75 @@ use std::collections::HashMap;
 
 #[derive(Serialize)]
 struct TemplateContext {
+    atom: Option<String>,
     question: Option<String>,
+    answers: Vec<String>,
+    context: Option<Context>,
+}
+
+#[derive(Serialize, Debug, Default)]
+struct Context {
+    atoms: Vec<String>,
     answers: Vec<String>,
 }
 
-// #[derive(FromForm)]
-// struct QuestionAnswerPair {
-//     question: String,
-//     answer: String,
-// }
+impl<'f> FromFormValue<'f> for Context {
+    type Error = (); // TODO Use more descriptive error.
+    fn from_form_value(item: &RawStr) -> Result<Self, ()> {
+        match item.percent_decode() {
+            Ok(context) => {
+                let mut atoms = Vec::new();
+                let mut answers = Vec::new();
+                for pairs in context.split_whitespace() {
+                    let mut iter = pairs.split(':').filter(|&w| w != "");
+                    let atom = iter.next();
+                    let answer = iter.next();
 
-// fn answer_question(inference_engine: &inference_engine::InferenceEngine, question:&Que)
+                    if let (Some(atom), Some(answer)) = (atom, answer) {
+                        atoms.push(atom.to_string());
+                        answers.push(answer.to_string());
+                    } else {
+                        return Err(());
+                    }
+                }
+                Ok(Context { atoms, answers })
+            }
+            Err(_) => Err(()),
+        }
+    }
+}
 
-fn get_next(questions: Vec<String>) -> Option<(String, Vec<String>)> {
+fn get_next(context: &Option<Context>) -> Option<(Question, Vec<String>)> {
     let path = "assets/knowledge_base/plumbing_knowledge_base.lms";
-    let inference_engine = inference_engine::prepare().with_knowledge_base_file(path);
+    let mut inference_engine = inference_engine::prepare().with_knowledge_base_file(path);
+
+    if let Some(context) = context {
+        for (atom, answer) in context.atoms.iter().zip(context.answers.clone()) {
+            let atom = Atom::new(atom.clone());
+            inference_engine.add_state(atom, &answer);
+        }
+    }
+
     if let Some(question) = inference_engine.next_question() {
-        Some((question.text, question.choices))
+        Some((question.clone(), question.choices))
+    } else {
+        None
+    }
+}
+
+fn get_answer(context: &Option<Context>) -> Option<String> {
+    let path = "assets/knowledge_base/plumbing_knowledge_base.lms";
+    let mut inference_engine = inference_engine::prepare().with_knowledge_base_file(path);
+
+    if let Some(context) = context {
+        for (atom, answer) in context.atoms.iter().zip(context.answers.clone()) {
+            let atom = Atom::new(atom.clone());
+            inference_engine.add_state(atom, &answer);
+        }
+    }
+
+    if let Some(answer) = inference_engine.reached_goal() {
+        Some(answer.text)
     } else {
         None
     }
@@ -46,29 +97,21 @@ fn index() -> Template {
     Template::render("index", &map)
 }
 
-#[get("/consult")]
-fn get() -> Template {
-    let context = if let Some((question, answers)) = get_next(Vec::new()) {
-        TemplateContext {
-            question: Some(question),
+#[get("/consult?<context>")]
+fn consult(context: Option<Context>) -> Template {
+    if let Some((question, answers)) = get_next(&context) {
+        let context = TemplateContext {
+            atom: Some(question.atom.text),
+            question: Some(question.text),
             answers,
-        }
+            context,
+        };
+        Template::render("question", &context)
     } else {
-        TemplateContext {
-            question: None,
-            answers: Vec::new(),
-        }
-    };
-    Template::render("question", &context)
-}
-
-#[get("/answer?<question>&<answer>")]
-fn answer(question: &RawStr, answer: &RawStr) -> String {
-    format!(
-        "Question: {}; Answer: {}",
-        question.as_str(),
-        answer.as_str()
-    )
+        let mut template_context = HashMap::new();
+        template_context.insert("answer", get_answer(&context));
+        Template::render("done", &template_context)
+    }
 }
 
 #[catch(404)] // Replace the default 404 with the definition below.
@@ -80,7 +123,7 @@ fn not_found(req: &Request) -> Template {
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![answer, redirect, index, get]) // Attach the routes specified above.
+        .mount("/", routes![redirect, index, consult]) // Attach the routes specified above.
         .mount(
             "/static",
             StaticFiles::from(format!("{}/assets/static", env!("CARGO_MANIFEST_DIR"))),
